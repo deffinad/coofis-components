aa6bec5dad64baceaf11c792b4a90ec7b181b410
ShawFox <fadlidwiramadhan@gmail.com>
2025-03-12 10:41:01 +0700

    Custom icon (icon / url)

--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,42 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Stack, Typography, IconButton, Tooltip } from '@mui/material';
 import * as MUIIcons from '@mui/icons-material';
 import { navbarListConfig } from '../navbarLayoutConfig';
 
+const CustomIcon = ({ menu }) => {
+    const [imageError, setImageError] = useState(false);
+    const IconComponent = MUIIcons[menu.icon] || MUIIcons['HelpOutline'];
+  
+    return !imageError && menu.image_icon ? (
+      <img
+        src={menu.image_icon}
+        onError={() => setImageError(true)}
+        width={24}
+        height={24}
+        alt="menu icon"
+      />
+    ) : (
+      <IconComponent fontSize="medium" />
+    );
+  };
+
 const Navbar = () => {
+
     return (
-        <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ width: '100%', background: 'black', padding: 2, borderRadius: 2 }}>
-            <Typography variant='h6' sx={{ fontWeight: 'bold', color: 'white' }}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ p: 2, background: 'black', borderRadius:2}}>
+            <Typography variant='h6' sx={{ fontWeight: 'bold', color: 'white', px: 2 }}>
                 Navbar  
             </Typography>
             
-            <Stack direction="row" gap={2}>
+            <Stack direction="row">
                 {navbarListConfig.map((menu, index) => {
-                       const IconComponent = MUIIcons[menu.icon];
                     
                     if (menu.active) {
                         return menu.clickable && menu.type === 'external' ? (
-                            <IconButton key={menu.id} sx={{ color: 'white' }} onClick={() => window.open(menu.external_url, '_blank')}>
-                                <IconComponent fontSize='medium' />
+                            <IconButton key={menu.id} title={menu.title} sx={{ color: 'white', '&:focus': { outline: 'none' }}} onClick={() => window.open(menu.external_url, '_blank')} >
+                                <CustomIcon menu={menu} />
                             </IconButton>
                         ) : menu.clickable ? (
-                            <IconButton key={menu.id} sx={{ color: 'white' }}>
-                                <IconComponent fontSize='medium' />
+                            <IconButton key={menu.id} title={menu.title} arrow sx={{ color: 'white', '&:focus': { outline: 'none' }}} >
+                                <CustomIcon menu={menu} />
                             </IconButton>
                         ) : (
                             <Tooltip key={menu.id} title={menu.title} arrow>
                                 <span> 
                                     <IconButton disabled sx={{ '&.Mui-disabled': { color: 'white'} }}>
-                                        <IconComponent fontSize='medium' />
+                                        <CustomIcon menu={menu} />
                                     </IconButton>
                                 </span>
                             </Tooltip>
                         );
                     }
-                    
                 })}
             </Stack>
-        </Stack>
+        </Stack>  
     );
 };
 
